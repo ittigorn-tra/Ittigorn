583db9c9da1c584d1073417727ec2b256e3f6860
Ittigorn Tradussadee <ittigorn.dhas@gmail.com>
2020-11-10 11:47:24 +0700

    Fixed operating system in classifier

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
   classifiers                   = [
     'Programming Language :: Python :: 3',
     'License :: OSI Approved :: MIT License',
-    'Operating Systems :: OS Independent',
+    'Operating System :: OS Independent',
   ],
   python_requires               = '>=3.7',
 )
